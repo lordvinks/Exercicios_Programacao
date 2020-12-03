15a0f17fecb67796e3e920d09bae3b19470d8511
lordvinks <68239414+lordvinks@users.noreply.github.com>
2020-12-03 00:19:35 -0300

    Criando exercicio 01

---
+++ Exercicios/exercicios.aulas.28a33/src/ex_01/README.md
@@ -0,0 +1,2 @@
+#LINK DOS ENUNCIADOS 
+https://www.slideshare.net/loianeg/curso-java-basico-exercicios-aulas-28-a-33
---
+++ Exercicios/exercicios.aulas.28a33/src/ex_01/TesteLampada.java
@@ -0,0 +1,11 @@
+package ex_01;
+
+public class TesteLampada {
+    
+
+    public static void main(String[] args){
+        Lampada lamp = new Lampada();
+
+        
+    }
+}
