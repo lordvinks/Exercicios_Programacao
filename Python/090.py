1b8f4ff31301c89d2d34069616d1958b0896c93c
Rafael Vizu <68239414+lordvinks@users.noreply.github.com>
2021-01-11 11:32:57 -0300

    Add files via upload

---
+++ Python/090.py
@@ -0,0 +1,19 @@
+print('\033[36;40mExercício Python #090 - Dicionário em Python\033[m')
+
+aluno = dict()
+print('=-=' * 10)
+
+aluno['nome'] = str(input('Nome: ')).capitalize().strip()
+aluno['nota 01'] = float(input('Nota 01: '))
+aluno['nota 02'] = float(input('Nota 02: '))
+
+print('=-=' * 10)
+
+aluno['media'] = (aluno['nota 01'] + aluno['nota 02']) / 2
+if aluno['media'] >= 7:
+    aluno['situação'] = 'aprovado'
+else:
+    aluno['situação'] = 'reprovado'
+
+for keys, values in aluno.items():
+    print(f'{keys.capitalize()}: {values}')
