2f083ff99c98b3946a3af1ff84d512919c86b8bd
Rafael Vizu <68239414+lordvinks@users.noreply.github.com>
2021-05-06 16:15:36 -0300

    add

--- Python/aulas-22_23/ex115/sistema.py
+++ Python/aulas-22_23/ex115/sistema.py
@@ -1,12 +1,23 @@
 from lib.interface import *
+from lib.arquivo import *
 from time import sleep
 
+arq = 'ex115.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
+
+
 while True:
-    resposta = menu(['Cadastrar Pessoas', 'Listar Pessoas', 'Sair do Sistema'])
+    resposta = menu(['Listar Pessoas', 'Cadastrar Pessoas', 'Sair do Sistema']) 
     if resposta == 1:
-        print()
+        # Opção de listar conteúdo de um arquivo!
+        lerArquivo(arq)
     elif resposta == 2:
-        print()
+        cabeçalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))    
+        idade = leiaInt('Idade: ')
+        cadastrar(arq, nome, idade)
     elif resposta == 3:
         print('Saindo do sistema... Até logo!')
     else:       
